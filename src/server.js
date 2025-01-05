a2a6fca7c023ab6d0d36e7ff8f973f6df992647d
Abdellah CHBIB <abdellahchbib20@gmail.com>
2025-01-05 11:39:33 +0000

    Question 3: Added error handling middleware and logging middleware

--- src/server.js
+++ src/server.js
@@ -10,10 +10,31 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
+// Middleware de logging
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url} - ${new Date().toISOString()}`);
+  next();
+});
+
 // Routes
 app.use('/api/news', newsRoutes);
 
-// TODO: Question 3 - Ajouter un middleware pour gérer les erreurs
+
+app.get('/test-error', (req, res, next) => {
+  const error = new Error('Ceci est une erreur de test.');
+  error.status = 400;
+  next(error);
+});
+
+
+// Middleware pour gérer les erreurs
+app.use((err, req, res, next) => {
+  console.error(err.stack); // Log l'erreur sur le serveur
+  res.status(500).json({
+      message: err.message || 'Une erreur est survenue sur le serveur.',
+  });
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
