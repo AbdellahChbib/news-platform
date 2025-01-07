f8677e70e65b7fc096d971a3c161b673a70d8315
Abdellah CHBIB <abdellahchbib20@gmail.com>
2025-01-07 14:33:28 +0000

    feat: Implemented routes for managing news articles (Question 4)

--- src/routes/newsRoutes.js
+++ src/routes/newsRoutes.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const newsController = require('../controllers/newsController');
 
-// TODO: Question 4 - Compléter les routes
-router.get('/', newsController.getAllNews);
-router.get('/:id', newsController.getNewsById);
-router.post('/', newsController.createNews);
+// Routes pour la gestion des articles
+router.get('/', newsController.getAllNews);         // Récupérer tous les articles
+router.get('/:id', newsController.getNewsById);     // Récupérer un article par ID
+router.post('/', newsController.createNews);        // Créer un nouvel article
+
+// Routes optionnelles pour la modification et la suppression
+router.put('/:id', newsController.updateNews);      // Modifier un article par ID
+router.delete('/:id', newsController.deleteNews);   // Supprimer un article par ID
 
 module.exports = router;
